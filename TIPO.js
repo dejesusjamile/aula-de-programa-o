1fef6f19f1f0c55b5bb3c5b0977d519ecc5cf28a
Jamile De Jesus <dejesusjamile315@gmail.com>
2024-06-13 11:49:46 -0300

    AULA DE PROGRAMAÇÃO

--- TIPO.js
+++ TIPO.js
@@ -1,3 +1,10 @@
-let fruta = "maçã"
+let fruta = "abacaxi"
 let frutas = ["banana, kiwi, abacaxi"] 
+let banana = {
+    "cor": "amarela",
+    "sabor": "doce",
+    "leguminosa": "não",
+    "fruta": "sim" ,
+    
+} 
 console.log(fruta + frutas) 
