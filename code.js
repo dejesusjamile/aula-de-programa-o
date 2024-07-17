93984888024ceee75f2651c214ed8151d31cb5d0
Jamile De Jesus <dejesusjamile315@gmail.com>
2024-07-17 12:13:55 -0300

    AULA DE INTERPRETAÇÃO

---
+++ code.js
@@ -0,0 +1,8 @@
+//quatro bolsos, dois primeiros mais fundos, primeiro bolso = celular, carteira, chave, segundo bolso = cadeado, outrachave, cartao de vista
+let calça = {
+    "bolso1": ["celular", "carteira", "chave"],
+    "bolso2": ["cadeado", "outrachave", "cartão de visita"],
+    "bolso3": null,
+    "bolso4": null
+}
+console.log (calça.bolso1 + calça.bolso2 + calça.bolso3 + calça.bolso4)
