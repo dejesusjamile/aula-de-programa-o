f6f3ded9e90c2ef381ce1ec36b10bed2b8dbedc1
Jamile De Jesus <dejesusjamile315@gmail.com>
2024-07-24 12:22:40 -0300

    aula de interpretação 2

--- code.js
+++ code.js
@@ -1,8 +1,20 @@
 //quatro bolsos, dois primeiros mais fundos, primeiro bolso = celular, carteira, chave, segundo bolso = cadeado, outrachave, cartao de vista
 let calça = {
-    "bolso1": ["celular", "carteira", "chave"],
-    "bolso2": ["cadeado", "outrachave", "cartão de visita"],
-    "bolso3": null,
-    "bolso4": null
+    "bolso1": {
+        "contem": ["carteira", "celular", "chave"],
+       " fundo": true
+    },
+    "bolso2": {
+    "contem": ["cadeado","outrachave", "cartaodevisita"],
+     "fundo": true
+    },
+    "bolso3": {
+        "contem": [],
+        "fundo": false
+    },
+    "bolso4":{
+        "contem": [],
+        "fundo": false
 }
-console.log (calça.bolso1 + calça.bolso2 + calça.bolso3 + calça.bolso4)
+}
+console.log ("Na minha calça temn" + Object.keys(calça) + "No bolso1, contem" + calça.bolso1.contem)
