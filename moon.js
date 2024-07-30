9543e446056dd81b4db87dccfbd1733885ad1a85
Jamile De Jesus <dejesusjamile315@gmail.com>
2024-07-30 12:28:14 -0300

    Aula de condicional

---
+++ moon.js
@@ -0,0 +1,12 @@
+let cinema = "tela"
+if(
+    cinema == "filme"
+) {
+    cinema = "true"
+}
+console.log(cinema)
+
+/*if é um comando 
+ (condição) é uma pergunta para ver ser vai ser atendida (true) ou não (false)
+  afirmação1 é o que vai acontecer caso a condição seja atendida
+ [else afirmação2] é o que vai acontecer caso a condicão não seja atendida*/
